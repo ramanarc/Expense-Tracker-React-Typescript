0b2c2def3a72225549c573262b9fd159d833c6a3
Anuj Raman <anujramanc@gmail.com>
2023-05-17 07:04:26 +0530

    adding html form

--- src/components/ExpenseInputForm.tsx
+++ src/components/ExpenseInputForm.tsx
@@ -3,32 +3,37 @@ import { Box, Button, FormControl, FormLabel, Input, NumberDecrementStepper, Num
 const ExpenseInputForm = () => {
   return (
     <Box p={4}>
-      <FormControl isRequired marginBottom={4}>
-        <FormLabel fontWeight='bold'>Description</FormLabel>
-        <Input placeholder='Enter expense description' />
-      </FormControl>
-      <FormControl isRequired marginBottom={4}>
-        <FormLabel fontWeight='bold'>Amount</FormLabel>
-        <NumberInput min={1}>
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-      </FormControl>
-      <FormControl isRequired marginBottom={4}>
-        <FormLabel fontWeight='bold'>What's the expense category?</FormLabel>
-        <Select>
-          <option>Groceries</option>
-          <option>Utilities</option>
-          <option>Entertainment</option>
-          <option>Housing</option>
-        </Select>
-      </FormControl>
-      <Button mt={4} colorScheme='blue' type='submit'>
-        Submit
-      </Button>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        console.log(e.target)
+      }}>
+        <FormControl isRequired marginBottom={4}>
+          <FormLabel fontWeight='bold'>Description</FormLabel>
+          <Input placeholder='Enter expense description' />
+        </FormControl>
+        <FormControl isRequired marginBottom={4}>
+          <FormLabel fontWeight='bold'>Amount</FormLabel>
+          <NumberInput min={1}>
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </FormControl>
+        <FormControl isRequired marginBottom={4}>
+          <FormLabel fontWeight='bold'>What's the expense category?</FormLabel>
+          <Select>
+            <option>Groceries</option>
+            <option>Utilities</option>
+            <option>Entertainment</option>
+            <option>Housing</option>
+          </Select>
+        </FormControl>
+        <Button mt={4} colorScheme='blue' type='submit'>
+          Submit
+        </Button>
+      </form>
     </Box>
   )
 }
